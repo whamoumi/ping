c5e7fca2d348ee3d66d5d07d05f8ce246b9be5d5
Votre Nom <Vous@exemple.com>
2025-08-24 17:31:39 +0200

    add -v option

--- Ping.c
+++ Ping.c
@@ -8,6 +8,24 @@
 #include <netinet/ip_icmp.h>
 #include <string.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <sys/time.h>
+
+struct info {
+    int sent;
+    int received;
+    double min;
+    double max;
+    double total;
+    char *hostname;
+    double rtt_min;
+    double rtt_max;
+    double rtt_sum;
+    double rtt_total;
+
+};
+
+struct info ping_info = {1, 0, 0.0, 0.0, 0.0, NULL, 0.0, 0.0, 0.0, 0.0};
 
 uint16_t checksum(void *b, int len) {
     uint16_t *buf = b;
@@ -21,59 +39,102 @@ uint16_t checksum(void *b, int len) {
     return ~sum;
 }
 
+double my_sqrt(double x)
+{
+    if (x <= 0)
+        return 0;
+    double guess = x / 2.0;
+    for (int i = 0; i < 10; i++) {  // 10 itérations suffisent pour une bonne précision
+        guess = (guess + x / guess) / 2.0;
+    }
+    return guess;
+}
+
+void sig_int(int sig)
+{
+    printf("\n--- %s ping statistics ---\n", ping_info.hostname);
+    printf("%d packets transmitted, %d received, %.1f%% packet loss\n",
+           ping_info.sent, ping_info.received,
+           (ping_info.sent - ping_info.received) * 100.0 / ping_info.sent);
+    if (ping_info.received > 0) {
+        // a revoir 
+        printf("rtt min/avg/max/mdev = %.3f/%.3f/%.3f/%.3f ms\n",
+                ping_info.rtt_min,
+                ping_info.rtt_sum / ping_info.received,
+                ping_info.rtt_max,
+                my_sqrt(ping_info.rtt_sum / ping_info.received - ping_info.rtt_min));
+    }
+    exit(0);
+}
+
 int main(int ac, char **av)
 {
     if (ac < 2 || ac > 3)
         return(write(1, "Error\n", 6));
-    struct hostent *hostinfo = NULL;
     struct in_addr **addr_list;
-    hostinfo = gethostbyname(av[1]); 
-    addr_list = (struct in_addr **)hostinfo->h_addr_list;
-    printf("PING %s (%s) 56(84) bytes of data.\n", av[1], inet_ntoa(*addr_list[0]));
     struct sockaddr_in dest;
     memset(&dest, 0, sizeof(dest));
     dest.sin_family = AF_INET;
-    dest.sin_addr = *addr_list[0];
-    int i = 1;
+    if (inet_aton(av[1], &dest.sin_addr) == 0)
+        return(write(1, "Give a valid IP address\n", 24));
+    ping_info.hostname = av[1];
+    printf("PING %s 56(84) bytes of data.\n", av[1]);
     while(1)
     {
+        struct timeval start, end;
         // creer le socket pour le protocole ICMP
         int sock = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
         if (sock == -1)
             return(write(1, "Error socket\n", 13)); 
-
+        struct timeval timeout;
+        timeout.tv_sec = 1;
+        timeout.tv_usec = 0;
+        setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
         // creer le paquet icmp pour lenvoie du socket     
         struct icmphdr *icmp_hdr = malloc(sizeof(struct icmphdr));
-        memset(icmp_hdr, 0, sizeof(struct icmp_hdr *));
+        memset(icmp_hdr, 0, sizeof(struct icmphdr));
         icmp_hdr->type = ICMP_ECHO; // 8
         icmp_hdr->code = 0;
         icmp_hdr->un.echo.id = getpid() & 0xFFFF;
-        icmp_hdr->un.echo.sequence = i;
+        icmp_hdr->un.echo.sequence = ping_info.sent;
         icmp_hdr->checksum = checksum(icmp_hdr, sizeof(struct icmphdr));
-
+        
         // envoie de ICMPECHO 
+        gettimeofday(&start, NULL);
         if (sendto(sock, icmp_hdr, sizeof(struct icmphdr), 0, (struct sockaddr *)&dest, sizeof(dest)) < 0)
             return(write(1, "Error socket 2 \n", 15));
-
+        
         //reception de ICMP Echo Reply
         struct sockaddr_in r_addr;
         char buffer[1024];
         socklen_t addr_len = sizeof(r_addr);
         struct icmphdr *icmp_hdr1 = malloc(sizeof(struct icmphdr));
         int n;
-        if ((n = recvfrom(sock, buffer, sizeof(buffer), 0, (struct sockaddr *)&r_addr, &addr_len)) < 0)
-            return(write(1, "Error socket 3 \n", 15));
-            struct iphdr *ip_hdr = (struct iphdr *)buffer;
-            int ip_header_len = ip_hdr->ihl * 4;
-            memcpy(icmp_hdr1, buffer + ip_header_len, sizeof(struct icmphdr));
-        printf("%d bytes from %s: icmp_seq=%d ttl=%d time=%.1f ms\n",
-               n, inet_ntoa(r_addr.sin_addr), icmp_hdr1->un.echo.sequence,
-               ip_hdr->ttl, (double)(i * 1000) );
+        n = recvfrom(sock, buffer, sizeof(buffer), 0, (struct sockaddr *)&r_addr, &addr_len);
+        
+        struct iphdr *ip_hdr = (struct iphdr *)buffer;
+        int ip_header_len = ip_hdr->ihl * 4;
+        memcpy(icmp_hdr1, buffer + ip_header_len, sizeof(struct icmphdr));
+        if (n > 0) {
+            gettimeofday(&end, NULL);
+            double rtt = (end.tv_sec - start.tv_sec) * 1000.0 + (end.tv_usec - start.tv_usec) / 1000.0;
+            if (ping_info.rtt_min == 0.0 || ping_info.rtt_min > rtt) {
+                ping_info.rtt_min = rtt;
+            }
+            if (ping_info.rtt_max < rtt) {
+                ping_info.rtt_max = rtt;
+            }
+            ping_info.rtt_sum += rtt;
+            ping_info.rtt_total += 1;
+            printf("%d bytes from %s: icmp_seq=%d ttl=%d time=%.2f ms\n",n, inet_ntoa(r_addr.sin_addr), icmp_hdr1->un.echo.sequence,ip_hdr->ttl, rtt);
+            ping_info.received++;
+        }
+        signal(SIGINT, &sig_int);
         free(icmp_hdr);
         free(icmp_hdr1);
         close(sock);
         sleep(1);
-        i++;
+        ping_info.sent++;
     }
     return (0);
 }
